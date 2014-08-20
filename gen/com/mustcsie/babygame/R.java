e86c87231e5b6fb9312b59758ac091f4df04c2e6
finch <pn546344@gmail.com>
2014-08-20 11:32:45 +0800

    新增遊戲初始畫面

--- gen/com/mustcsie/babygame/R.java
+++ gen/com/mustcsie/babygame/R.java
@@ -22,10 +22,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int baby=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int lift_button=0x7f020002;
+        public static final int right_button=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080006;
+        public static final int editText1=0x7f080005;
+        public static final int imageButton1=0x7f080001;
+        public static final int imageButton2=0x7f080003;
+        public static final int imageView1=0x7f080002;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
